a508e8e3fc362038600772de6939777c319912db
Hmue Thet Paing Kyaw <94979276+hmuethetpaingkyaw@users.noreply.github.com>
2023-07-08 08:51:59 +0630

    add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	  import Navbar from "$lib/components/navbar/Navbar.svelte";
 import "../app.css";
+import Analytics from '$lib/analytics.svelte'
 </script>
 
 
-	
+<Analytics />
+
 		<Navbar />
 	
 	<main class="w-full h-screen bg-white">
